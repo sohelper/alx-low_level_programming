6290d7f53c16858a5bdd0dc9291b0ef1d0c4e3cb
root <saharbadr2508@gmail.com>
2023-06-15 14:32:03 -0700

    ASDAS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - error
- * Return:0
+ * Return:1
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,\
-	       	2015-10-19\n");
+char soo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(1,soo,59);
 
 return (1);
 }
