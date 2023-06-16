db5b98086c33568fc78b432fff4b09e01e017901
root <saharbadr2508@gmail.com>
2023-06-16 15:58:41 -0700

    sss

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - numbers digits
+ * Return:0
+ */
+int main(void)
+{
+int num;
+for (num = 0 ; num < 10 ; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
