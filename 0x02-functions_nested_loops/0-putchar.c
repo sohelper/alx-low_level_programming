cee597815fde93b477848d99e8e9fde219fbca1f
root <saharbadr2508@gmail.com>
2023-06-20 14:31:42 -0700

    sssss

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  * Description: prints _putchar, followed by a new line.
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet  -  alphabet game
+ * Return:0
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchat('\n');
+return (0);
+}
