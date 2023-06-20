3deeb476e76c967e2b95bc7f80ecd5a7239cfc74
root <saharbadr2508@gmail.com>
2023-06-20 16:33:18 -0700

    WER

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *  print_sign -  prints the sign of a number
- *  @n is integer number
+ *  @n: is integer number
  *  Return:1  is greater than zero
  *         0  is zero
  *         -1  is less than zero
