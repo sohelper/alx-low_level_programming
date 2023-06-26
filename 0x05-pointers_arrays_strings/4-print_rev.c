b0493b604f849dbdd1078fe3a43ffea72b32a9b1
root <saharbadr2508@gmail.com>
2023-06-26 14:41:25 -0700

    ssss

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev -  function that prints a string, in reverse
+ * @s:string that print
+ * Return :nothing
+ */
+void print_rev(char *s)
+{
+int len;
+for (len = 0; s[len] != '\0')
+{
+len++
+}
+int y;
+for (y = len; y >= 0; y--)
+{
+_putchar(s[y]);
+}
+_putchar('\n');
+}
