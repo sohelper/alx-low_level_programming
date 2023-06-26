5150c55cbd653fea581c3b8692c52855924fe76e
root <saharbadr2508@gmail.com>
2023-06-26 07:44:35 -0700

    aaaa

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main,h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s:string
+ * Return:lenght of array
+ */
+int _strlen(char *s)
+{
+int x;
+x = 0;
+while (s[x] != '\0')
+{
+x++;
+}
+return (x);
+}
