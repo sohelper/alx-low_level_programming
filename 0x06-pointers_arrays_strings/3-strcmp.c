f02765f4e72dbca133726746f10c9b5699032288
root <saharbadr2508@gmail.com>
2023-06-29 13:01:19 -0700

    hggfx

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings.
+ * @s1:array one
+ * @s2:array two
+ * Return:(0)
+ */
+int _strcmp(char *s1, char *s2)
+{
+int y = 0;
+while (*s1 == *s2 && *s1 != '\0')
+{
+s1++;
+s2++;
+}
+if (s1 != s2}
+{
+y = *s1 - *s2;
+}
+return (y);
+}
