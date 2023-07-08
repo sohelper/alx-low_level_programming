45e8426f9cce31fab7d10052eac360fb94637de5
root <saharbadr2508@gmail.com>
2023-07-07 17:53:56 -0700

    hhh

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print
+ * @argc:count
+ * @argv:array
+ *Return:0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0, i < argc, i++)
+printf("%s\n", argv[i]);
+return (0);
+}
