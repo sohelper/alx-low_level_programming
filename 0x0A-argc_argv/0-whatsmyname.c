fb9cb4d71c6b39df3c05d9ea36112f25cf218f7a
root <saharbadr2508@gmail.com>
2023-07-07 17:18:09 -0700

    hhh

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stio.h>
+/**
+ * main - main function that print name
+ * @argv:value of array
+ * @argc:count number of element
+ * Return:0
+ */
+int main(__attribute__((unused)), argc, char *argv[])
+{
+printf("%s\n" ,argv[0]);
+return (0);
+}
