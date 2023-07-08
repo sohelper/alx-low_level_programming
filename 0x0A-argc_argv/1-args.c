d5a97d2045883de70e7867bd30b9898e63b92753
root <saharbadr2508@gmail.com>
2023-07-07 17:42:41 -0700

    hhh

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function that print number of array
+ * @argc:count number of argument
+ * @argv:array of argument
+ *Return:0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
