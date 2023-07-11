0396e0719b8432e9e4ca31f83a8d940d16af2c67
root <saharbadr2508@gmail.com>
2023-07-11 14:06:55 -0700

    ddddd

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,12 +17,12 @@ return (NULL);
 }
 for (len = 0; str[len] != '\0'; len++)
 ;
-sahar = malloc(sizeof(char) * (len + 1);
+sahar = malloc(sizeof(char) * (len + 1));
 if (sahar == 0)
 {
 return (NULL);
 }
 for (i = 0; i <= len; i++)
 sahar[i] = str[i];
-return (sahar);
+return sahar;
 }
