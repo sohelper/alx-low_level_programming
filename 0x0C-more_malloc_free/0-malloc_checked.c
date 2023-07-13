4fc4272d553aadd308754c664dbb97d80b5fb560
root <saharbadr2508@gmail.com>
2023-07-13 12:50:08 -0700

    jjj

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked -  function that allocates memory using malloc
+ * @b:unsigned int
+ * Return: value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+int x;
+void *x = malloc(b);
+if (x == NULL)
+{
+return (98);
+}
+else
+{
+return (p);
+}
+}
