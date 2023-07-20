0699249189aa0f9e1ed6233ab87c1bd8467ea1de
root <saharbadr2508@gmail.com>
2023-07-19 18:38:42 -0700

    vvvvv

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
 *array_iterator - executes a function on each element of an array
 *@array: array to be targeted
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
 *int_index - searches for an integer
 *@array: Array containing elements
