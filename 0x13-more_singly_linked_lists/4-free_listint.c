bdfabf8c38f50875f989cb6d3098db6d48dab634
root <saharbadr2508@gmail.com>
2023-07-31 13:46:55 -0700

    kkkk

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+*free_listint -linked list
+*@head: head of list
+*Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
