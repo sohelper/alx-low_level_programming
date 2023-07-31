a3cade4250919dc88bf5f4d8711de9b7abae454b
root <saharbadr2508@gmail.com>
2023-07-31 14:15:13 -0700

    kkkk

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
+size_t num = 0;
 
-while (cursor != NULL)
+while (h)
 {
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
+printf("%d\n", h->n);
+num++;
+h = h->next;
 }
-return (count);
+return (num);
 }
