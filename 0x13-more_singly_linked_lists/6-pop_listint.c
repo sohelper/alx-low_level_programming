3e7c307a62731757aaa52aa50db6b49a70f30bbd
root <saharbadr2508@gmail.com>
2023-07-31 14:21:04 -0700

    kkkk

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,16 +8,15 @@
 
 int pop_listint(listint_t **head)
 {
-listint_t *popped;
-int content;
+listint_t *temp;
+int num;
 
-if (*head == NULL)
+if (!head || !*head)
 return (0);
-popped = *head;
-content = popped->n;
-free(popped);
 
-*head = (*head)->next;
-return (content);
-
+num = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+return (num);
 }
