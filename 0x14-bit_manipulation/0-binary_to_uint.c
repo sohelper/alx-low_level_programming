dd842bd6393acee59a702f0913b7292c89378732
sohelper <sohelper2508@gmail.com>
2023-09-29 12:02:47 -0700

    welcome back

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,19 +1,27 @@
+
 #include "main.h"
 
-unsigned int str_to_uint(const char *str) 
+/**
+ * binary_to_uint - this function converts binary to int
+ * @b: str with binary number
+ *
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
 {
-  int i;
-  unsigned int dec = 0;
+	int i;
+	unsigned int val = 0;
 
-  if (!str)
-    return (0);
+	if (!b)
+		return (0);
 
-  for(i = 0; str[i]; i++)
-  {
-    if(str[i] < '0' || str[i] > '1')
-      return (0);
-    dec = 2 * dec + (str[i] - '0'); 
-  }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		val = 2 * val + (b[i] - '0');
+	}
 
-  return (dec);
+	return (val);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-unsigned int str_to_uint(const char *str);
-void print_bin(unsigned long int n);
-int get_index(unsigned long int n, unsigned int i); 
-int set_index(unsigned long int *n, unsigned int i);
-
+unsigned int binary_to_uint(const char *b);
 #endif
