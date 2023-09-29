56684d98517d85e7bb096ed7e30fc22c4a07e271
sohelper <sohelper2508@gmail.com>
2023-09-29 12:05:10 -0700

    again we are doing it

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_binary - this function show the binary representation.
+ * @n: the number you are wishing to show.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
