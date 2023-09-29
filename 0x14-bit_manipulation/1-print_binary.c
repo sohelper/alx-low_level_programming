f51365c9204cc647783f58f394337d85db02ecc0
sohelper <sohelper2508@gmail.com>
2023-09-29 12:25:40 -0700

    ya rab

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,10 +7,14 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n >> 0)
 	{
-		print_binary(n >> 1);
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-
-	_putchar((n & 1) + '0');
+	else
+	{
+		_putchar('0');
+	}
 }
