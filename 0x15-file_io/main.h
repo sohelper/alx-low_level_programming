fd27ff0e678853f82c2bd542ccdf02fb4635a973
sohelper <sohelper2508@gmail.com>
2023-10-02 12:33:26 -0700

    tany

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
